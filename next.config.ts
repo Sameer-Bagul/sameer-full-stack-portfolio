3b37345babcfd575d16d4bd5aa6240bb41f648ab
Sameer-Bagul <sameerbagul2004@gmail.com>
2026-02-27 18:44:25 +0530

    icons issue  solved

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'skillicons.dev',
         port: '',
         pathname: '/**',
       },
